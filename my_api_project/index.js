ea63ab431a50008e3cf913ea05d225a4572c1421
BLANLEUIL Louis <111581406+HakiosR@users.noreply.github.com>
2024-12-18 14:01:16 +0100

    adding api_key

--- my_api_project/index.js
+++ my_api_project/index.js
@@ -1,10 +1,18 @@
+require('dotenv').config();
+
 const express = require("express")
 const axios   = require("axios")
 const app     = express()
 
+const apiKey = process.env.API_KEY;
+
 app.get("/external-data", async (req, res) => {
     try {
-        const response = await axios.get("https://api.thecatapi.com/v1/images/search?limit=3&page=1&order=RANDOM")
+        const response = await axios.get("https://api.thecatapi.com/v1/images/search?limit=3&page=1&order=RANDOM", {
+            headers: {
+                'x-api-key': "apiKey"  // Remplacez par ta clé API
+            }
+        })
         res.json(response.data);
     } catch (e) {
         res.status(500).send(e.message);
@@ -15,7 +23,7 @@ app.get("/get-breeds", async (req, res) => {
     try {
         const response = await axios.get("https://api.thecatapi.com/v1/breeds", {
             headers: {
-                'x-api-key': 'YOUR_API_KEY'  // Remplacez par ta clé API
+                'x-api-key': "apiKey"  // Remplacez par ta clé API
             }
         })
         res.json(response.data);
@@ -26,7 +34,11 @@ app.get("/get-breeds", async (req, res) => {
 
 app.get("/get-categories", async (req, res) => {
     try {
-        const response = await axios.get("https://api.thecatapi.com/v1/categories")
+        const response = await axios.get("https://api.thecatapi.com/v1/categories", {
+            headers: {
+                'x-api-key': "apiKey"  // Remplacez par ta clé API
+            }
+        })
         res.json(response.data);
     } catch (e) {
         res.status(500).send(e.message);
