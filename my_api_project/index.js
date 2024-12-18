bb383c9905ed0dfd5d8072dfd5846e51a8bc2f51
BLANLEUIL Louis <111581406+HakiosR@users.noreply.github.com>
2024-12-18 14:08:21 +0100

    Update API_KEY

--- my_api_project/index.js
+++ my_api_project/index.js
@@ -1,48 +1,47 @@
-require('dotenv').config();
+const express = require("express");
+const axios = require("axios");
+const app = express();
 
-const express = require("express")
-const axios   = require("axios")
-const app     = express()
-
-const apiKey = process.env.API_KEY;
+// Récupération de la clé API depuis l'environnement de CircleCI
+const apiKey = process.env.API_KEY; // Pas besoin de dotenv
 
 app.get("/external-data", async (req, res) => {
     try {
         const response = await axios.get("https://api.thecatapi.com/v1/images/search?limit=3&page=1&order=RANDOM", {
             headers: {
-                'x-api-key': apiKey  // Remplacez par ta clé API
+                'x-api-key': apiKey
             }
-        })
+        });
         res.json(response.data);
     } catch (e) {
         res.status(500).send(e.message);
     }
-})
+});
 
 app.get("/get-breeds", async (req, res) => {
     try {
         const response = await axios.get("https://api.thecatapi.com/v1/breeds", {
             headers: {
-                'x-api-key': apiKey  // Remplacez par ta clé API
+                'x-api-key': apiKey 
             }
-        })
+        });
         res.json(response.data);
     } catch (e) {
         res.status(500).send(e.message);
     }
-})
+});
 
 app.get("/get-categories", async (req, res) => {
     try {
         const response = await axios.get("https://api.thecatapi.com/v1/categories", {
             headers: {
-                'x-api-key': apiKey  // Remplacez par ta clé API
+                'x-api-key': apiKey  
             }
-        })
+        });
         res.json(response.data);
     } catch (e) {
         res.status(500).send(e.message);
     }
-})
+});
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"))
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
